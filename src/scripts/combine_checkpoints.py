e1cd6aae8db71c85622c029497ce4c8099d7607f
crisostomi <donatocrisostomi@gmail.com>
2024-06-25 10:51:32 +0200

    add function to combine checkpoints into one

---
+++ src/scripts/combine_checkpoints.py
@@ -0,0 +1,38 @@
+import torch
+
+
+def combine(
+    generator_checkpoint: str, detector_checkpoint: str, output_checkpoint: str
+):
+    """Combine split generator and detector checkpoints into a single checkpoint that can be further trained."""
+    gen_ckpt = torch.load(generator_checkpoint)
+    det_ckpt = torch.load(detector_checkpoint)
+
+    combined_ckpt = {
+        "xp.cfg": gen_ckpt["xp.cfg"],  # assuming the configs are identical
+        "model": {},
+    }
+
+    # add generator layers with appropriate prefix
+    for layer in gen_ckpt["model"].keys():
+        new_layer = f"generator.{layer}"
+        combined_ckpt["model"][new_layer] = gen_ckpt["model"][layer]
+
+    # add detector layers with appropriate prefix
+    for layer in det_ckpt["model"].keys():
+        new_layer = f"detector.{layer}"
+        combined_ckpt["model"][new_layer] = det_ckpt["model"][layer]
+
+    # special case for 'msg_processor.msg_processor.weight'
+    if "msg_processor.msg_processor.weight" in gen_ckpt["model"]:
+        combined_ckpt["model"]["msg_processor.msg_processor.0.weight"] = gen_ckpt[
+            "model"
+        ]["msg_processor.msg_processor.weight"]
+
+    torch.save(combined_ckpt, output_checkpoint)
+
+
+if __name__ == "__main__":
+    import fire
+
+    fire.Fire(combine)
