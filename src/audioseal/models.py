99bf44e1a22d75802650510995450cadfc0e3bc1
Tuan Tran <tuantran@devfair0436.h2.fair>
2025-12-09 12:02:03 +0000

    lint

--- src/audioseal/models.py
+++ src/audioseal/models.py
@@ -4,10 +4,10 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
-from contextlib import contextmanager
 import functools
 import logging
 import sys
+from contextlib import contextmanager
 from typing import Optional, Tuple
 
 import torch
